82c3707c6e109d8cd7a341c3ae28d02da70edd6e
Nymphium <s1311350@gmail.com>
2024-04-01 22:13:02 +0900

    fixup! build with maven

--- src/main.java
+++ src/main.java
@@ -10,22 +10,24 @@ import java.util.function.Function;
 
 // Implicitly Declared Classes and Instance Main Methods
 // (Previously "Unnamed Classes and Instance Main Methods" Java 21, 2nd preview) - Java 22
-void main() {
-  var id = abs("x", var("x"));
-  System.out.println(id);
+public class Main {
+  void main() {
+    var id = abs("x", var("x"));
+    System.out.println(id);
 
-  var two = abs("f", abs("x", app(var("f"), app(var("f"), var("x")))));
-  var three = abs("f", abs("x", app(var("f"), app(var("f"), app(var("f"), var("x"))))));
-  var plus = abs("m", abs("n", abs("f", abs("x", app(app(var("m"), var("f")), app(app(var("n"), var("f")), var("x")))))));
-  builtin1("print", app(app(plus, two), three)).eval(new ArrayList<>());
+    var two = abs("f", abs("x", app(var("f"), app(var("f"), var("x")))));
+    var three = abs("f", abs("x", app(var("f"), app(var("f"), app(var("f"), var("x"))))));
+    var plus = abs("m", abs("n", abs("f", abs("x", app(app(var("m"), var("f")), app(app(var("n"), var("f")), var("x")))))));
+    builtin1("print", app(app(plus, two), three)).eval(new ArrayList<>());
 
-  var print = new Pair<String,Lambda<String>>("print",
-          nativefn(a -> {
-              System.out.println(a);
-              return unit();
-          }));
+    var print = new Pair<String,Lambda<String>>("print",
+            nativefn(a -> {
+                System.out.println(a);
+                return unit();
+            }));
 
-  var env = List.of(print);
+    var env = List.of(print);
 
-  app(var(print.first()), app(app(plus, two), three)).eval(env);
+    app(var(print.first()), app(app(plus, two), three)).eval(env);
+  }
 }
